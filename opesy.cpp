c08a90479baf84ba6f853e9b550f24239dc18d1f
Samantha O'Neil <122878620+sam-oneil@users.noreply.github.com>
2025-06-13 21:41:34 +0800

    updated fcfs implementation and running processes

--- opesy.cpp
+++ opesy.cpp
@@ -299,42 +299,57 @@ public:
         : ready_queue(ready), running_list(running), cpu_cores(cores), shutdown_signal(shutdown) {}
 
     void run()
+{
+    while (!shutdown_signal)
     {
-        while (!shutdown_signal)
+        bool assigned;
+        if (!ready_queue.isEmpty())
         {
-            auto current_running = running_list.get_all();
-            running_list.processes.clear();
-
+            // Wait until a core is idle 
             for (auto *core : cpu_cores)
             {
-                std::lock_guard<std::mutex> lock(core->core_mtx);
-                if (core->current_process != nullptr)
+                if (core->is_idle()) // if there is a core
                 {
-                    Process updated_proc = *(core->current_process);
-                    updated_proc.status = RUNNING;
-                    running_list.add(updated_proc);
-                }
-            }
-
-            for (auto *core : cpu_cores)
-            {
-                if (core->is_idle())
-                {
-                    Process p_to_run("", -1);
-                    if (ready_queue.pop(p_to_run))
+                    Process next_process("", -1);
+                    if (ready_queue.pop(next_process))
                     {
-                        p_to_run.assigned_core_id = core->get_id();
-                        if (core->assign_process(p_to_run))
+                        next_process.assigned_core_id = core->get_id();
+                        next_process.status = RUNNING;
+                        if (core->assign_process(next_process))
                         {
-                            running_list.add(p_to_run);
+                            running_list.add(next_process);
+                            assigned = true;
                         }
                     }
+                    break;  // one process at a time for FCFS
+                }
+                else {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 }
             }
+        }
+        else
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        {
+            std::lock_guard<std::mutex> lock(running_list.mtx);
+            running_list.processes.clear();  // clear running processes list
+
+            for (auto *core : cpu_cores)
+            {
+                std::lock_guard<std::mutex> core_lock(core->core_mtx);
+                if (core->current_process != nullptr && core->current_process->status != FINISHED)
+                {
+                    running_list.processes.push_back(*core->current_process);
+                }
+            }
         }
     }
+}
+
+
 };
 
 void printBanner()
