6553c6f2ecf360857ec04ac71a490d86f33060d3
Sam Oneil <122878620+sam-oneil@users.noreply.github.com>
2025-07-12 21:40:19 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -178,6 +178,7 @@ int main()
     std::thread reporter_thread(memory_reporter_thread);
     std::string input, screenName;
     bool initialized = false;
+    int exit = 0;
     int process_id_counter = 1;
 
     std::cout << "\033[32mSystem initialized successfully.\033[0m\n";
@@ -214,7 +215,10 @@ int main()
             std::cout << "  clear         - Clear the screen" << std::endl;
             std::cout << "  exit          - Exit the application." << std::endl;
         }
-        else
+        else if (input == "exit")
+        {
+            exit = 9;
+        }
         {
             std::cout << "\033[31mUnknown Command. Please run 'initialize' first.\033[0m" << std::endl;
         }
@@ -222,147 +226,149 @@ int main()
     } while (!initialized);
 
     // now that it has been initialized
-    bool exit = false;
-    do
+    if (exit != 9)
     {
-        std::cout << "\033[36m" << "Command> " << "\033[0m";
-        std::getline(std::cin, input);
+        do
+        {
+            std::cout << "\033[36m" << "Command> " << "\033[0m";
+            std::getline(std::cin, input);
 
-        if (input == "help")
-        {
-            std::cout << "  screen -ls    - Lists all running and finished processes." << std::endl;
-            std::cout << "  screen -s <name> - Create a new screen/process." << std::endl;
-            std::cout << "  screen -r <name> - Resume/view an existing screen." << std::endl;
-            std::cout << "  scheduler-start - Start the scheduler." << std::endl;
-            std::cout << "  scheduler-stop - Stop the scheduler." << std::endl;
-            std::cout << "  report-util   - Generate utilization report." << std::endl;
-            std::cout << "  clear         - Clear the screen" << std::endl;
-            std::cout << "  exit          - Stops all threads and exits the application." << std::endl;
-        }
-        else if (input == "clear")
-        {
-            system("cls");
-            std::cout.flush();
-            printBanner();
-        }
-        else if (input == "initialize")
-        {
-            std::cout << "System already initialized." << std::endl;
-        }
-        else if (input.rfind("screen", 0) == 0)
-        {
-            if (input == "screen -ls")
+            if (input == "help")
+            {
+                std::cout << "  screen -ls    - Lists all running and finished processes." << std::endl;
+                std::cout << "  screen -s <name> - Create a new screen/process." << std::endl;
+                std::cout << "  screen -r <name> - Resume/view an existing screen." << std::endl;
+                std::cout << "  scheduler-start - Start the scheduler." << std::endl;
+                std::cout << "  scheduler-stop - Stop the scheduler." << std::endl;
+                std::cout << "  report-util   - Generate utilization report." << std::endl;
+                std::cout << "  clear         - Clear the screen" << std::endl;
+                std::cout << "  exit          - Stops all threads and exits the application." << std::endl;
+            }
+            else if (input == "clear")
             {
                 system("cls");
-                // system("clear");
-                std::stringstream report;
-                // CPU Usage
-                int busy_cores = 0;
-                for (const auto &p : g_running_list.get_all())
-                {
-                    if (p.status == RUNNING)
-                    {
-                        busy_cores++;
-                    }
-                }
-                float utilization = (g_config.num_cpu > 0) ? (static_cast<float>(busy_cores) / g_config.num_cpu) * 100.0f : 0.0f;
-                report << "CPU Utilization: " << std::fixed << std::setprecision(2) << utilization << "%\n";
-                report << "Cores Used: " << busy_cores << " / " << g_config.num_cpu << "\n\n";
-
-                std::cout << report.str();
-                std::cout << "\n";
-                std::cout << "--------------------------------------------------------\n";
-
-                std::cout << "Running processes:\n";
-                auto running = g_running_list.get_all();
-                if (running.empty())
-                {
-                    std::cout << "  (None)\n";
-                }
-                else
-                {
-                    for (const auto &p : running)
-                    {
-                        std::cout << "  " << p.name << "\t(" << p.creation_timestamp << ")\t"
-                                  << "Core: " << p.assigned_core_id << "\t"
-                                  << p.commandCounter << " / " << p.totalCommands;
-
-                        std::cout << "\n";
-                    }
-                }
-
-                std::cout << "\n";
-                std::cout << "\n";
-                std::cout << "Finished processes:\n";
-                auto finished = g_finished_list.get_all();
-                if (finished.empty())
-                {
-                    std::cout << "  (None)\n";
-                }
-                else
-                {
-                    for (const auto &p : finished)
-                    {
-                        std::cout << "  " << p.name << "\t(" << p.creation_timestamp << ")\t"
-                                  << "Finished\t"
-                                  << p.commandCounter << " / " << p.totalCommands << "\n";
-                    }
-                }
-                std::cout << "--------------------------------------------------------\n\n";
+                std::cout.flush();
+                printBanner();
             }
-            else if (input.rfind("screen -s ", 0) == 0)
+            else if (input == "initialize")
             {
-                screenName = input.substr(10);
-                // Check if a process with this name already exists in any list
-                if (g_ready_queue.exists(screenName) || g_running_list.exists(screenName) || g_finished_list.exists(screenName))
+                std::cout << "System already initialized." << std::endl;
+            }
+            else if (input.rfind("screen", 0) == 0)
+            {
+                if (input == "screen -ls")
                 {
-                    std::cout << "Process or screen \"" << screenName << "\" already exists.\n";
+                    system("cls");
+                    // system("clear");
+                    std::stringstream report;
+                    // CPU Usage
+                    int busy_cores = 0;
+                    for (const auto &p : g_running_list.get_all())
+                    {
+                        if (p.status == RUNNING)
+                        {
+                            busy_cores++;
+                        }
+                    }
+                    float utilization = (g_config.num_cpu > 0) ? (static_cast<float>(busy_cores) / g_config.num_cpu) * 100.0f : 0.0f;
+                    report << "CPU Utilization: " << std::fixed << std::setprecision(2) << utilization << "%\n";
+                    report << "Cores Used: " << busy_cores << " / " << g_config.num_cpu << "\n\n";
+
+                    std::cout << report.str();
+                    std::cout << "\n";
+                    std::cout << "--------------------------------------------------------\n";
+
+                    std::cout << "Running processes:\n";
+                    auto running = g_running_list.get_all();
+                    if (running.empty())
+                    {
+                        std::cout << "  (None)\n";
+                    }
+                    else
+                    {
+                        for (const auto &p : running)
+                        {
+                            std::cout << "  " << p.name << "\t(" << p.creation_timestamp << ")\t"
+                                    << "Core: " << p.assigned_core_id << "\t"
+                                    << p.commandCounter << " / " << p.totalCommands;
+
+                            std::cout << "\n";
+                        }
+                    }
+
+                    std::cout << "\n";
+                    std::cout << "\n";
+                    std::cout << "Finished processes:\n";
+                    auto finished = g_finished_list.get_all();
+                    if (finished.empty())
+                    {
+                        std::cout << "  (None)\n";
+                    }
+                    else
+                    {
+                        for (const auto &p : finished)
+                        {
+                            std::cout << "  " << p.name << "\t(" << p.creation_timestamp << ")\t"
+                                    << "Finished\t"
+                                    << p.commandCounter << " / " << p.totalCommands << "\n";
+                        }
+                    }
+                    std::cout << "--------------------------------------------------------\n\n";
+                }
+                else if (input.rfind("screen -s ", 0) == 0)
+                {
+                    screenName = input.substr(10);
+                    // Check if a process with this name already exists in any list
+                    if (g_ready_queue.exists(screenName) || g_running_list.exists(screenName) || g_finished_list.exists(screenName))
+                    {
+                        std::cout << "Process or screen \"" << screenName << "\" already exists.\n";
+                    }
+                    else
+                    {
+                        Process new_process(screenName, process_id_counter++, g_config);
+                        g_ready_queue.push(new_process);
+
+                        screens.emplace(screenName, [=]()
+                                        { Console::display(screenName, g_ready_queue, g_running_list, g_finished_list); });
+
+                        screens[screenName]();
+                    }
+                }
+                else if (input.rfind("screen -r ", 0) == 0)
+                {
+                    screenName = input.substr(10);
+                    Console::display(screenName, g_ready_queue, g_running_list, g_finished_list);
                 }
                 else
                 {
-                    Process new_process(screenName, process_id_counter++, g_config);
-                    g_ready_queue.push(new_process);
-
-                    screens.emplace(screenName, [=]()
-                                    { Console::display(screenName, g_ready_queue, g_running_list, g_finished_list); });
-
-                    screens[screenName]();
+                    std::cout << "\033[31m" << "Enter 'screen -ls' to list processes, 'screen -s <name>' to create, or 'screen -r <name>' to resume." << "\033[0m" << std::endl;
                 }
             }
-            else if (input.rfind("screen -r ", 0) == 0)
+            else if (input == "scheduler-start")
             {
-                screenName = input.substr(10);
-                Console::display(screenName, g_ready_queue, g_running_list, g_finished_list);
+                g_generate_processes = true;
+                std::cout << "Automatic process generation started." << std::endl;
+            }
+            else if (input == "scheduler-stop")
+            {
+                g_generate_processes = false;
+                std::cout << "Automatic process generation stopped." << std::endl;
+            }
+            else if (input == "report-util")
+            {
+                generate_report();
+            }
+            else if (input == "exit")
+            {
+                exit = 1;
             }
             else
             {
-                std::cout << "\033[31m" << "Enter 'screen -ls' to list processes, 'screen -s <name>' to create, or 'screen -r <name>' to resume." << "\033[0m" << std::endl;
+                std::cout << "\033[31m" << "Command not recognized. Type [help] for available commands." << "\033[0m" << std::endl;
             }
-        }
-        else if (input == "scheduler-start")
-        {
-            g_generate_processes = true;
-            std::cout << "Automatic process generation started." << std::endl;
-        }
-        else if (input == "scheduler-stop")
-        {
-            g_generate_processes = false;
-            std::cout << "Automatic process generation stopped." << std::endl;
-        }
-        else if (input == "report-util")
-        {
-            generate_report();
-        }
-        else if (input == "exit")
-        {
-            exit = true;
-        }
-        else
-        {
-            std::cout << "\033[31m" << "Command not recognized. Type [help] for available commands." << "\033[0m" << std::endl;
-        }
 
-    } while (!exit);
+        } while (exit != 1);
+    }
 
     std::cout << "\n\033[33m[System] Shutdown initiated. Waiting for all tasks to complete...\033[0m\n";
     g_shutdown = true;
